a22d49ca2272a382bb7157e40b1e14309a26749e
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-06 22:28:44 +0530

    feat: Implement module-based documentation structure
    Transitioned from a single overview page to a structured, module-based documentation system. This enhances clarity and navigation by providing dedicated pages for each module category.

--- theme.config.tsx
+++ theme.config.tsx
@@ -40,24 +40,4 @@ export default {
       </span>
     ),
   },
-  Layout({ children, pageOpts }: NextraThemeLayoutProps) {
-    const { title, frontMatter, headings } = pageOpts;
-
-    return (
-      <div>
-        <Head>
-          <title>{title}</title>
-          <meta name="og:image" content={frontMatter.image} />
-        </Head>
-        <h1>My Theme</h1>
-        Table of Contents:
-        <ul>
-          {headings.map((heading) => (
-            <li key={heading.value}>{heading.value}</li>
-          ))}
-        </ul>
-        <div style={{ border: "1px solid" }}>{children}</div>
-      </div>
-    );
-  },
 };

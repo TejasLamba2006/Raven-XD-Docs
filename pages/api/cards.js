6f59459a3bd3a653e94728fc6695566d0550c746
TejasLamba2006 <tejas22feb@gmail.com>
2024-07-07 14:10:07 +0530

    feat: Update directory paths for module API calls
    Updates the directory paths in the module API calls to fix the issue where the paths were not correctly joined. Previously, the paths were joined with forward slashes ("/"), but now they are joined with backslashes ("\") to ensure the correct directory is accessed when fetching module data. This change improves the reliability and accuracy of the API calls.

--- pages/api/cards.js
+++ pages/api/cards.js
@@ -1,17 +1,17 @@
-// pages/api/cards.js
+// ..&pages/api/cards.js
 import fs from 'node:fs/promises';
 import path from 'node:path';
 const transformString = (input) => input.replace(/([A-Z])/g, (_match, p1, offset) => offset > 0 ? ` ${p1}` : p1);
 
 export default async function handler(req, res) {
-    const directoryPath = path.join("../../", req.query.path.replaceAll(".", "/"));
+    const directoryPath = path.join(req.query.path);
     try {
         const files = await fs.readdir(directoryPath);
         const cards = files
             .filter(file => path.extname(file) === '.mdx')
             .map(file => ({
                 title: transformString(file.slice(0, -4)),
-                href: `/${req.query.path.replace("pages.", "").replaceAll(".", "/")}/${file.slice(0, -4)}`,
+                href: `/${req.query.path.replace("..&pages.", "").replaceAll(".", "/")}/${file.slice(0, -4)}`,
             }));
         res.status(200).json(cards);
     } catch (err) {
